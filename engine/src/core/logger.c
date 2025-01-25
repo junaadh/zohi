9fe26f6eda084799e22af333828a81197a7e346c
Moosa Junaadh <junaadh.02@gmail.com>
2025-01-26 05:01:43 +0800

    add windows and linux basic windows

--- engine/src/core/logger.c
+++ engine/src/core/logger.c
@@ -1,10 +1,11 @@
 #include "logger.h"
 #include "asserts.h"
+#include "platform/platform.h"
 
 // TODO: temp
+#include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdarg.h>
 
 void report_assertion_failure(const char *expression, const char *msg, const char *file, i32 line) {
     log_output(LOG_LEVLEL_FATAL, "Assertion Failure: %s, message: '%s', in file: %s, line: %d\n", expression, msg, file, line);
@@ -19,13 +20,14 @@ void shutdown_logging() {
     // TODO: cleanup logging/write queued entries
 }
 
-void log_output(log_level level, const char* msg, ...) {
-    const char* lvls[6] = { "[FATAL]: ", "[ERROR]: ", "[WARN]: ", "[INFO]: ", "[DEBUG]: ", "[TRACE]: " };
-    // b8 is_error = level < 2;
+void log_output(log_level level, const char *msg, ...) {
+    const char *lvls[6] = {"[FATAL]: ", "[ERROR]: ", "[WARN]: ", "[INFO]: ", "[DEBUG]: ", "[TRACE]: "};
+    b8 is_error = level < LOG_LEVEL_WARN;
 
     // create a buffer of 32k characters
     // DONT DO THIS
-    char out_msg[32000];
+    const i32 msg_len = 32000;
+    char out_msg[msg_len];
     memset(out_msg, 0, sizeof(out_msg));
 
     // format original msg
@@ -33,12 +35,16 @@ void log_output(log_level level, const char* msg, ...) {
     // therefore the workaround is to use __builtin_va_list which is the type gcc/clang's va_start expects
     __builtin_va_list arg_ptr;
     va_start(arg_ptr, msg);
-    vsnprintf(out_msg, 32000, msg, arg_ptr);
+    vsnprintf(out_msg, msg_len, msg, arg_ptr);
     va_end(arg_ptr);
 
-    char final[32000];
+    char final[msg_len];
     sprintf(final, "%s%s\n", lvls[level], out_msg);
 
-    // TODO: platform specific output
-    printf("%s", final);
+    // platform specific output
+    if (is_error) {
+        platform_console_write_error(final, level);
+    } else {
+        platform_console_write(final, level);
+    }
 }
--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,14 +1,22 @@
 #include <core/logger.h>
 #include <core/asserts.h>
+#include <platform/platform.h>
 
 int main(void) {
-    ZFATAL("FUCK, %f", 6.9f);
-    ZERROR("FUCK, %f", 6.9f);
-    ZWARN("FUCK, %f", 6.9f);
-    ZINFO("FUCK, %f", 6.9f);
-    ZDEBUG("FUCK, %f", 6.9f);
-    ZTRACE("FUCK, %f", 6.9f);
+    ZFATAL("HELLO, %f", 6.9f);
+    ZERROR("HELLO, %f", 6.9f);
+    ZWARN("HELLO, %f", 6.9f);
+    ZINFO("HELLO, %f", 6.9f);
+    ZDEBUG("HELLO, %f", 6.9f);
+    ZTRACE("HELLO, %f", 6.9f);
 
-    ZASSERT(1 == 69);
+    platform_state state;
+    if (platform_startup(&state, "Zohi Testbed", 100, 100, 1200, 720)) {
+        while (TRUE) {
+            platform_pump_messages(&state);
+        }
+    }
+    platform_shutdown(&state);
+    
     return 0;
 }
